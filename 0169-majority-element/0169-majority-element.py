620fe29be9db0d469ff7f8471741b60346df14dc
Nabonita Roy <nabonitaroy15345@gmail.com>
2026-05-14 14:59:09 +0530

    Time: 15 ms (19.26%), Space: 21.2 MB (52.15%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count={}
+        n=len(nums)
+        for i in nums:
+            if i not in count:
+                count[i]=1
+            else:
+                count[i]+=1
+            if count[i]>n/2:
+                return i
+
+
+        
+
+        
