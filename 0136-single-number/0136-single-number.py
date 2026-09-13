570db77cb4816bcaa523caca1b20dad0a4b9f4ee
Nabonita Roy <nabonitaroy15345@gmail.com>
2026-09-13 06:49:23 +0530

    Time: 0 ms (100%), Space: 21.3 MB (30.07%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        prev={}
-        for i  in nums:
-            if i not in prev:
-                prev[i]=1
-            else:
-                prev[i]+=1
-        for n in nums:
-            if prev[n]==1:
-                return n
+        ans=0
+        for i in nums:
+            ans^=i
+        return ans
 
